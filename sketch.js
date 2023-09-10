f87683d7c4ebd3e095f5b940509015155ee68aa2
Wiley Wiggins <weevil@wileywiggins.com>
2023-09-10 14:47:38 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,12 +10,134 @@ let sounds = [];
 let season = 0;
 let day = 0;
 
+const BOX_TOP_LEFT = xyToIndex(8, 9);
+const BOX_HORIZONTAL = xyToIndex(11, 8);
+const BOX_VERTICAL = xyToIndex(17, 7);
+const BOX_TOP_RIGHT = xyToIndex(6, 8);  
+const BOX_BOTTOM_LEFT = xyToIndex(5, 1);
+const BOX_BOTTOM_RIGHT = xyToIndex(7, 9);
+const BOX_VERTICAL_HORIZONTAL = xyToIndex(12,8);
+const BOX_UP_HORIZONTAL = xyToIndex(8,8);
+const BOX_LEFT_VERTICAL = xyToIndex(18,7);
+const BOX_RIGHT_VERTICAL = xyToIndex(10,8);
+const BOX_DOWN_HORIZONTAL = xyToIndex(9,8);
+const BOX_HORIZONTAL_HALF = xyToIndex(20,10);
+
+
+
 function preload() {
   // Assuming the image is named 'spritesheet.png' and is in the same directory
   spritesheet = loadImage('spritesheet.png');
   // Load sounds
   for (let i = 1; i <= 22; i++) {
     sounds.push(loadSound('sounds/' + i + '.wav'));
+  }
+}
+
+function xyToIndex(x, y) {
+  return y * SPRITESHEET_COLS + x;
+}
+
+function isBoxTile(val) {
+  return [
+    BOX_TOP_LEFT, BOX_HORIZONTAL, BOX_VERTICAL, 
+    BOX_TOP_RIGHT, BOX_BOTTOM_LEFT, BOX_BOTTOM_RIGHT,
+    BOX_VERTICAL_HORIZONTAL, BOX_UP_HORIZONTAL, BOX_LEFT_VERTICAL,
+    BOX_RIGHT_VERTICAL, BOX_DOWN_HORIZONTAL, BOX_HORIZONTAL_HALF
+  ].includes(val);
+}
+
+function addConnectingTile(i, j, updatedGrid) {
+  let randChoice = (arr) => arr[floor(random(arr.length))];
+
+  if (grid[i][j] === BOX_TOP_LEFT) {
+    if (i+1 < GRID_WIDTH && !isBoxTile(grid[i+1][j])) {
+      updatedGrid[i+1][j] = randChoice([BOX_HORIZONTAL, BOX_VERTICAL_HORIZONTAL,BOX_TOP_RIGHT,]);
+    }
+    if (j+1 < GRID_HEIGHT && !isBoxTile(grid[i][j+1])) {
+      updatedGrid[i][j+1] = randChoice([BOX_VERTICAL, BOX_BOTTOM_LEFT, BOX_VERTICAL_HORIZONTAL]);
+    }
+  }
+
+  if (grid[i][j] === BOX_HORIZONTAL) {
+    if (i+1 < GRID_WIDTH && !isBoxTile(grid[i+1][j])) {
+      updatedGrid[i+1][j] = randChoice([BOX_HORIZONTAL, BOX_TOP_RIGHT, BOX_BOTTOM_RIGHT, BOX_VERTICAL_HORIZONTAL]);
+    }
+    if (i-1 >= 0 && !isBoxTile(grid[i-1][j])) {
+      updatedGrid[i-1][j] = randChoice([BOX_HORIZONTAL, BOX_TOP_LEFT, BOX_BOTTOM_LEFT, BOX_VERTICAL_HORIZONTAL]);
+    }
+  }
+
+  if (grid[i][j] === BOX_VERTICAL) {
+    if (j+1 < GRID_HEIGHT && !isBoxTile(grid[i][j+1])) {
+      updatedGrid[i][j+1] = randChoice([BOX_VERTICAL, BOX_TOP_LEFT, BOX_TOP_RIGHT, BOX_VERTICAL_HORIZONTAL]);
+    }
+    if (j-1 >= 0 && !isBoxTile(grid[i][j-1])) {
+      updatedGrid[i][j-1] = randChoice([BOX_VERTICAL, BOX_BOTTOM_LEFT, BOX_BOTTOM_RIGHT, BOX_VERTICAL_HORIZONTAL]);
+    }
+  }
+
+  // ... Similar logic for BOX_TOP_RIGHT, BOX_BOTTOM_LEFT, and BOX_BOTTOM_RIGHT ...
+  if (grid[i][j] === BOX_TOP_RIGHT) {
+    if (i-1 >= 0 && !isBoxTile(grid[i-1][j])) {
+      updatedGrid[i-1][j] = randChoice([BOX_HORIZONTAL, BOX_TOP_LEFT, BOX_VERTICAL_HORIZONTAL]);
+    }
+    if (j+1 < GRID_HEIGHT && !isBoxTile(grid[i][j+1])) {
+      updatedGrid[i][j+1] = randChoice([BOX_VERTICAL, BOX_BOTTOM_LEFT, BOX_VERTICAL_HORIZONTAL]);
+    }
+  } 
+  if (grid[i][j] === BOX_BOTTOM_LEFT) {
+    if (i+1 < GRID_WIDTH && !isBoxTile(grid[i+1][j])) {
+      updatedGrid[i+1][j] = randChoice([BOX_HORIZONTAL, BOX_TOP_RIGHT, BOX_VERTICAL_HORIZONTAL]);
+    }
+    if (j-1 >= 0 && !isBoxTile(grid[i][j-1])) {
+      updatedGrid[i][j-1] = randChoice([BOX_VERTICAL, BOX_BOTTOM_RIGHT, BOX_VERTICAL_HORIZONTAL]);
+    }
+  }
+  if (grid[i][j] === BOX_BOTTOM_RIGHT) { 
+    if (i-1 >= 0 && !isBoxTile(grid[i-1][j])) {
+      updatedGrid[i-1][j] = randChoice([BOX_HORIZONTAL, BOX_TOP_LEFT, BOX_VERTICAL_HORIZONTAL]);
+    }
+    if (j-1 >= 0 && !isBoxTile(grid[i][j-1])) {
+      updatedGrid[i][j-1] = randChoice([BOX_VERTICAL, BOX_BOTTOM_LEFT, BOX_VERTICAL_HORIZONTAL]);
+    }
+  }
+
+  if (grid[i][j] === BOX_VERTICAL_HORIZONTAL) {
+    if (i+1 < GRID_WIDTH && !isBoxTile(grid[i+1][j])) {
+      updatedGrid[i+1][j] = randChoice([BOX_HORIZONTAL, BOX_TOP_RIGHT, BOX_BOTTOM_RIGHT]);
+    }
+    if (i-1 >= 0 && !isBoxTile(grid[i-1][j])) {
+      updatedGrid[i-1][j] = randChoice([BOX_HORIZONTAL, BOX_TOP_LEFT, BOX_BOTTOM_LEFT]);
+    }
+    if (j+1 < GRID_HEIGHT && !isBoxTile(grid[i][j+1])) {
+      updatedGrid[i][j+1] = randChoice([BOX_VERTICAL, BOX_TOP_LEFT, BOX_TOP_RIGHT]);
+    }
+    if (j-1 >= 0 && !isBoxTile(grid[i][j-1])) {
+      updatedGrid[i][j-1] = randChoice([BOX_VERTICAL, BOX_BOTTOM_LEFT, BOX_BOTTOM_RIGHT]);
+    }
+  }
+}
+
+
+function hasBoxTiles() {
+  for (let i = 0; i < GRID_WIDTH; i++) {
+    for (let j = 0; j < GRID_HEIGHT; j++) {
+      if (isBoxTile(grid[i][j])) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function reseedGrid() {
+  for (let i = 0; i < GRID_WIDTH; i++) {
+    for (let j = 0; j < GRID_HEIGHT; j++) {
+      if (!isBoxTile(grid[i][j])) {
+        grid[i][j] = floor(random(SPRITESHEET_COLS * SPRITESHEET_ROWS));
+     }
+    }
   }
 }
 
@@ -42,26 +164,32 @@ function draw() {
    season = 0;
   }
   // Create a copy of the grid to store updates, so we aren't reading and writing from the same grid simultaneously.
+  
   let updatedGrid = [];
   for (let i = 0; i < GRID_WIDTH; i++) {
     updatedGrid[i] = grid[i].slice();
   }
 
+  if (!hasBoxTiles()) {
+    reseedGrid();
+  }
+
   for (let i = 0; i < GRID_WIDTH; i++) {
     for (let j = 0; j < GRID_HEIGHT; j++) {
       let val = grid[i][j];
-
+      if ((!isBoxTile(val) || (isBoxTile(val) && !canConnect(i, j))) && !isBoxTile(updatedGrid[i][j])) {
+        updatedGrid[i][j] = (val + 1) % (SPRITESHEET_COLS * SPRITESHEET_ROWS);
+      }
       // If the current tile has a value of 6, set all its neighbors to 6
-      if (val == 6 - season) {
+      if (val == 8 - season) {
         for (let dx = -1; dx <= 1; dx++) {
-          for (let dy = -1; dy <= 1; dy++) {
-            let ni = i + dx;
-            let nj = j + dy;
-
-            if (ni >= 0 && nj >= 0 && ni < GRID_WIDTH && nj < GRID_HEIGHT) {
-              updatedGrid[ni][nj] = 6;
+            for (let dy = -1; dy <= 1; dy++) {
+                let ni = i + dx;
+                let nj = j + dy;
+                if (ni >= 0 && nj >= 0 && ni < GRID_WIDTH && nj < GRID_HEIGHT) {
+                    updatedGrid[ni][nj] = 6;
+                }
             }
-          }
         }
       }
 
@@ -69,10 +197,12 @@ function draw() {
       drawTile(i, j, val);
       
       // Increment the value for the next frame
-      updatedGrid[i][j] = (val + 1) % (SPRITESHEET_COLS * SPRITESHEET_ROWS);
-      if (i == 0 && j == 0 && (day == 1 || day == 4) ) {
-        let soundIndex = val % 22;
-        sounds[soundIndex].play();
+      if (!isBoxTile(val) || (isBoxTile(val) && !canConnect(i, j))) {
+        updatedGrid[i][j] = (val + 1) % (SPRITESHEET_COLS * SPRITESHEET_ROWS);
+        if (i == 0 && j == 0 && (day == 1 || day == 4) ) {
+          let soundIndex = val % 22;
+          sounds[soundIndex].play();
+        }
       }
     }
   }
@@ -87,7 +217,24 @@ function draw() {
   }
 
   grid = updatedGrid;
-  frameRate(100);
+  frameRate(30);
+}
+
+function canConnect(i, j) {
+  const directions = [
+    {dx: 1, dy: 0},
+    {dx: -1, dy: 0},
+    {dx: 0, dy: 1},
+    {dx: 0, dy: -1}
+  ];
+  for (const dir of directions) {
+    let ni = i + dir.dx;
+    let nj = j + dir.dy;
+    if (ni >= 0 && nj >= 0 && ni < GRID_WIDTH && nj < GRID_HEIGHT && !isBoxTile(grid[ni][nj])) {
+      return true;
+    }
+  }
+  return false;
 }
 
 function drawTile(i, j, val) {
