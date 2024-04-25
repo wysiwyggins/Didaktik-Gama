b4535d055176d9e62c478e105e2d500c464fac75
Wiley Wiggins <weevil@wileywiggins.com>
2024-04-25 10:26:29 -0500

    path to an image worked locally but not on pi

--- public/scripts/boot.js
+++ public/scripts/boot.js
@@ -33,7 +33,7 @@ function preload() {
 
   spritesheet = loadImage('assets/spritesheets/libuse40x30-cp437.png');
   spritesheetData = loadJSON('assets/spritesheets/spriteData.json');
-  backgroundImage = loadImage(`/public/assets/images/boot.png`);
+  backgroundImage = loadImage('assets/images/boot.png');
   
 }
 
