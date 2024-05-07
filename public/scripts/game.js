64f3dbd65dacd249695e195e8d620b7e27417d64
Wiley Wiggins <weevil@wileywiggins.com>
2024-05-07 12:12:45 -0500

    accidentally broke sound, fixed

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -188,7 +188,7 @@ PIXI.Loader.shared.onComplete.add(() => {
 });
 //console.log(smokeFrames);
 
-fetch("./data/grottoAudiosprite.json")
+fetch("data/grottoAudiosprite.json")
   .then(response => response.json())
   .then(data => {
     audioSpriteData = data;
