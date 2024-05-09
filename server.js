5835c4b5a2d8b37a29972545c8fc870428a5381c
Wiley Wiggins <weevil@wileywiggins.com>
2024-05-09 16:12:02 -0500

    troubleshooting io error on the pi

--- server.js
+++ server.js
@@ -1,3 +1,35 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const open = require('open');
+const os = require('os');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);  // Make sure io is initialized here
+
+// DMX setup only on Linux
+let universe;
+if (os.platform() === 'linux') {
+    const DMX = require('dmx');
+    const dmx = new DMX();
+    universe = dmx.addUniverse('demo', 'enttec-usb-dmx-pro', '/dev/ttyUSB1');
+}
+
+// Define a variable to hold judgeName
+let judgeName = '';
+
+// Serve static files from the public directory
+app.use(express.static('public'));
+
+// Start server and automatically open the default browser
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+    open(`http://localhost:${PORT}`);
+});
+
+// WebSocket connection setup
 io.on('connection', (socket) => {
     console.log('New client connected');
 
@@ -7,7 +39,6 @@ io.on('connection', (socket) => {
         io.emit('updateJudgeName', { judgeName });
     });
 
-    // Listen for baseColor changes
     socket.on('baseColorChanged', (data) => {
         const { red, green, blue } = data;
         console.log(`Base color changed to Red: ${red}, Green: ${green}, Blue: ${blue}`);
@@ -37,4 +68,21 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         console.log('Client disconnected');
     });
+});
+
+function setRGBLight(red, green, blue) {
+    if (universe) {
+        universe.update({1: red, 2: green, 3: blue});
+    }
+}
+
+function setBlackLight(state) {
+    if (universe) {
+        universe.update({4: state ? 255 : 0});
+    }
+}
+
+// Expose judgeName through a route
+app.get('/judgeName', (req, res) => {
+    res.send({ judgeName });
 });
